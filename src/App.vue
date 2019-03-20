b24d828b0c5fdea5b966ba4f0cf0a0856a8bdd69
jim <192389590@qq.com>
2019-03-20 18:15:10 +0800

    写编辑器

--- src/App.vue
+++ src/App.vue
@@ -17,10 +17,35 @@ export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-#app {
-    font-family: "microsoft yahei", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #333;
+@import "./resource/style/index";
+.flex {
+    display: flex;
+    &.center {
+        align-items: center;
+    }
+    &.column{
+        flex-direction: column;
+    }
+    .flex1 {
+        flex: 1;
+    }
+}
+pre {
+    font-family: Consolas, "Courier New", monospace;
+    .string {
+        color: #e6db74;
+    }
+    .number {
+        color: #ae81ff;
+    }
+    .boolean {
+        color: #ae81ff;
+    }
+    .null {
+        color: #f92672;
+    }
+    .key {
+        color: #a6e22e;
+    }
 }
 </style>
--- src/components/post.vue
+++ src/components/post.vue
@@ -1,6 +1,23 @@
 <template>
-    <div class="pages_post">
-        <h1>{{ pageName }}</h1>
+    <div class="pages_post flex column">
+        <div class="post_hd">
+            <div class="hd_tool flex center">
+                <div class="hd_left flex center flex1">
+                    <div class="hd_method">{{postForm.method}}</div>
+                    <input class="hd_url flex1" v-model="postForm.url">
+                </div>
+                <div class="hd_send">Send</div>
+            </div>
+        </div>
+        <div class="post_bd flex1 flex">
+            <div class="bd_req flex1">
+                <pre class="text_req" contenteditable="true" v-html="reqData"></pre>
+                <!-- <textarea class="text_req" name="text_req" id="text_req" cols="30" rows="10" v-model="reqData"></textarea> -->
+            </div>
+            <div class="bd_res flex1 scrollbar">
+                <pre v-html="resData"></pre>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,6 +27,10 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 @Component
 export default class Pages_post extends Vue {
     pageName: string = "pages_post";
+    postForm: any = {
+        method: "POST",
+        url: "http://172.21.0.21:3001/"
+    };
     postBody: any = {
         mock: {
             respCode: "0000",
@@ -37,6 +58,12 @@ export default class Pages_post extends Vue {
         }
     };
     resBody: any = {};
+    get reqData(): string {
+        return this.inputFormat(this.postBody);
+    }
+    get resData(): string {
+        return this.syntaxHighlight(this.postBody);
+    }
     mounted() {
         this.post();
     }
@@ -66,11 +93,145 @@ export default class Pages_post extends Vue {
                 console.log("fetch fail", JSON.stringify(e));
             });
     }
+    inputFormat(json): string {
+        if (typeof json != "string") {
+            json = JSON.stringify(json, undefined, 2);
+        }
+        json = json
+            .replace(/&/g, "&")
+            .replace(/</g, "<")
+            .replace(/>/g, ">");
+        return json.replace(
+            /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+            function(match) {
+                var cls = "number";
+                if (/^"/.test(match)) {
+                    if (/:$/.test(match)) {
+                        cls = "key";
+                    } else {
+                        cls = "string";
+                    }
+                } else if (/true|false/.test(match)) {
+                    cls = "boolean";
+                } else if (/null/.test(match)) {
+                    cls = "null";
+                }
+                return '<span class="' + cls + '">' + match + "</span>";
+            }
+        );
+    }
+    syntaxHighlight(json): string {
+        if (typeof json != "string") {
+            json = JSON.stringify(json, undefined, 2);
+        }
+        json = json
+            .replace(/&/g, "&")
+            .replace(/</g, "<")
+            .replace(/>/g, ">");
+        return json.replace(
+            /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+            function(match) {
+                var cls = "number";
+                if (/^"/.test(match)) {
+                    if (/:$/.test(match)) {
+                        cls = "key";
+                    } else {
+                        cls = "string";
+                    }
+                } else if (/true|false/.test(match)) {
+                    cls = "boolean";
+                } else if (/null/.test(match)) {
+                    cls = "null";
+                }
+                return '<span class="' + cls + '">' + match + "</span>";
+            }
+        );
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='scss' scoped>
 .pages_post {
+    height: 100vh;
+    padding: 20px;
+    .post_hd {
+        .hd_tool {
+            height: 40px;
+            .hd_left {
+                border-radius: 5px;
+                border: 1px solid #666;
+                background: #404040;
+                box-sizing: content-box;
+                overflow: hidden;
+                .hd_method,
+                .hd_url {
+                    line-height: 40px;
+                    text-indent: 10px;
+                    &:active,
+                    &:hover,
+                    &:focus {
+                        background: #282828;
+                        outline: 0;
+                    }
+                }
+                .hd_method {
+                    cursor: pointer;
+                    width: 100px;
+                    border-right: 1px solid #666;
+                }
+                .hd_url {
+                    color: #eee;
+                    background: #404040;
+                    border: 0;
+                    box-sizing: border-box;
+                }
+            }
+            .hd_send {
+                border-radius: 5px;
+                box-sizing: content-box;
+                overflow: hidden;
+                width: 80px;
+                line-height: 40px;
+                background: #1890ff;
+                margin-left: 10px;
+                text-align: center;
+                cursor: pointer;
+                user-select: none;
+                &:hover,
+                &:focus {
+                    background: #40a9ff;
+                }
+                &:active,
+                &.active {
+                    background-color: #096dd9;
+                }
+            }
+        }
+    }
+    .post_bd {
+        margin-top: 10px;
+        .bd_req,
+        .bd_res {
+            overflow: auto;
+            border-radius: 5px;
+            border: 1px solid #404040;
+            background: #282828;
+            padding: 20px;
+            .text_req {
+                border: 0;
+                color: #fff;
+                background: #282828;
+                width: 100%;
+                height: 100%;
+                &:active,
+                &:hover,
+                &:focus {
+                    background: #282828;
+                    outline: 0;
+                }
+            }
+        }
+    }
 }
 </style>
