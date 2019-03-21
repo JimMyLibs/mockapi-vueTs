24c69885432526ff9f6409f2ab44d0a25db6f861
jim <192389590@qq.com>
2019-03-21 15:17:04 +0800

    添加报错返回信息

--- src/components/post.vue
+++ src/components/post.vue
@@ -10,9 +10,9 @@
             </div>
         </div>
         <div class="post_bd flex1 flex">
-            <div class="bd_req flex1 flex column">
+            <div class="bd_req flex1">
                 <pre class="text_req" contenteditable="true" v-html="reqData"></pre>
-                <textarea class="text_req" name="text_req" id="text_req" cols="30" rows="10" @keydown.tab="inputTab($event)" v-model="reqBody"></textarea>
+                <textarea class="text_req" name="text_req" id="text_req" @keydown.tab="inputTab($event)" v-model="reqBody"></textarea>
             </div>
             <div class="bd_res flex1 scrollbar">
                 <pre contenteditable="true" v-html="resData"></pre>
@@ -24,6 +24,17 @@
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 
+Vue.config.errorHandler = function (err, vm, info) {
+    vm.$data.resBody = {
+        '异常信息': err.message,
+        '异常名称': err.name,
+        '异常脚本url': err.script,
+        '异常行号': err.line,
+        '异常列号': err.column,
+        '异常堆栈信息': err.stack,
+        'handle ': info,
+    };
+}
 @Component({
     directives: {}
 })
@@ -31,7 +42,7 @@ export default class Pages_post extends Vue {
     pageName: string = "pages_post";
     postForm: any = {
         method: "POST",
-        url: "http://172.21.0.21:3001/"
+        url: "http://mock.91525.net:35001/"
     };
     reqBody: any = '{\n\t"mock": {\n\t\t"id":"id"\n\t}\n}';
     resBody: any = {};
@@ -54,7 +65,7 @@ export default class Pages_post extends Vue {
                 JSON.parse(this.reqBody.replace(/\n/g,'').replace(/\t/g,''))
             )
         })
-            .then(data => {
+            .then((data: any) => {
                 // in some SAMSUNG mobile data.ok is undefined so add data.status
                 if (data.ok || data.status === 200) {
                     return data.json();
@@ -63,33 +74,42 @@ export default class Pages_post extends Vue {
                     throw new Error(`响应数据异常，错误码：${data.status}`);
                 }
             })
-            .then(data => {
+            .then((data: any) => {
                 this.resBody = data;
                 return data;
             })
-            .catch(function(e) {
-                console.log("fetch fail", JSON.stringify(e));
+            .catch((error: any) => {
+                if (error.response) {
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    console.log(error.request);
+                } else {
+                    console.log('Error', error.message);
+                }
+                console.log(error.config);
+                this.resBody = JSON.stringify(error);
+                return error;
             });
     }
-    inputTab(e): void {
+    inputTab(e: any): void {
         let selectionStart = e.target.selectionStart;
         let selectionEnd = e.target.selectionEnd;
         let pos = selectionStart + 2;
-        console.log("tab", pos, selectionStart, selectionEnd);
         e.target.value = e.target.value.substr(0, selectionStart) + "\t" + e.target.value.substr(selectionStart);
         e.target.selectionStart = e.target.selectionEnd = pos-1;
         e.target.focus();
-        console.log("tab2", pos, selectionStart, selectionEnd);
         e && e.preventDefault();
     }
-    inputFormat(json): string {
+    inputFormat(json: any): string {
         if (typeof json != "string") {
             json = JSON.stringify(json, undefined, 4);
         }
         json = json.replace(/,/g, ",");
         return json;
     }
-    syntaxHighlight(json): string {
+    syntaxHighlight(json: any): string {
         if (typeof json != "string") {
             json = JSON.stringify(json, undefined, 4);
         }
@@ -99,7 +119,7 @@ export default class Pages_post extends Vue {
             .replace(/>/g, ">");
         return json.replace(
             /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
-            function(match) {
+            function(match: any) {
                 var cls = "number";
                 if (/^"/.test(match)) {
                     if (/:$/.test(match)) {
@@ -207,6 +227,8 @@ export default class Pages_post extends Vue {
                 background: transparent;
                 letter-spacing: 0.4px;
                 line-height: 1.7;
+                overflow: hidden;
+                margin-left: 2px;
             }
         }
     }
